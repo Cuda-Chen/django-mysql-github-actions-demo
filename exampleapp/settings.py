ebf6a33547c9fc5f1dba8da20f864ad31209d2a8
Cuda-Chen <clh960524@gmail.com>
2021-05-28 16:06:02 +0800

    add app users and GitHub Actions CI
    1. Add app `users`.
    2. Add GitHub Actions CI.

--- exampleapp/settings.py
+++ exampleapp/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,10 +77,6 @@ WSGI_APPLICATION = 'exampleapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    'test': {
         'ENGINE': os.environ.get('DBENGINE', ''),
         'NAME': os.environ.get('DBNAME', ''),
         'USER': os.environ.get('DBUSER', ''),
@@ -88,6 +86,12 @@ DATABASES = {
     }
 }
 
+"""DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
---
+++ users/tests.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+class UserTestCase(TestCase):
+    def test_user(self):
+        username = 'cudachen'
+        password = 'carbotzergling'
+        u = User(username=username)
+        u.set_password(password)
+        u.save()
+        self.assertEqual(u.username, username)
+        self.assertTrue(u.check_password(password))
